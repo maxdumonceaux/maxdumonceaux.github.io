0ad03e904a5f411d240e77b19365fddafcfffd52
Max Dumonceaux <maxdumonceaux@gmail.com>
2025-08-09 12:12:11 -0700

    Some basic styling and content in place.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const cleanCss = require('gulp-clean-css')
 const sourcemaps = require('gulp-sourcemaps')
+const browserSync = require('browser-sync').create()
 
 gulp.task('scss', () => gulp.src('web/assets/scss/*.scss')
     .pipe(sourcemaps.init())
@@ -9,9 +10,14 @@ gulp.task('scss', () => gulp.src('web/assets/scss/*.scss')
     .pipe(cleanCss({ compatibility: 'ie9' }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('web/assets/dist'))
+    .pipe(browserSync.stream())
 )
 
 gulp.task('watch', cb => {
+    browserSync.init({
+        proxy: 'local.website.com'
+    })
+
     gulp.watch([
         'web/assets/scss/**/*.scss',
         'web/assets/scss/*.scss'
